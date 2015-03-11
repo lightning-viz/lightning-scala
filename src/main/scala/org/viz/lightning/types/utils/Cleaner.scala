6d74955fd259029847b9a16c77e5393e1acc7370
freeman <the.freeman.lab@gmail.com>
2015-03-11 14:22:03 -0400

    Restore type checking, remove options

--- src/main/scala/org/viz/lightning/types/utils/Cleaner.scala
+++ src/main/scala/org/viz/lightning/types/utils/Cleaner.scala
@@ -4,7 +4,15 @@ import scala.reflect.runtime.universe._
 
 class Cleaner[T: TypeTag](func: T => Map[String, Any]) {
 
+  def check[K: TypeTag](): Unit = {
+    if (!(typeOf[K] =:= typeOf[T])) {
+      throw new Exception("Data type %s does not match expected type %s"
+        .format(typeOf[K], typeOf[T]))
+    }
+  }
+
   def apply[K: TypeTag](data: K): Map[String, Any] = {
+    check[K]()
     func(data.asInstanceOf[T])
   }
 
