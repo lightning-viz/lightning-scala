72900a09325e51f9f176352af64439a7bfbbbe9c
freeman <the.freeman.lab@gmail.com>
2015-03-11 01:45:36 -0400

    Add URL methods

--- src/main/scala/org/viz/lightning/Visualization.scala
+++ src/main/scala/org/viz/lightning/Visualization.scala
@@ -5,3 +5,34 @@ class Visualization(lightning: Lightning, id: Int) {
 
 
 }
+
+  def formatURL(url: String): String = {
+    val out = url.last.toString match {
+      case "/" => url
+      case _ => url + "/"
+    }
+    out + "?host=" + lgn.host
+  }
+
+  def getPermalinkURL: String = {
+    lgn.host + "/visualizations/" + id
+  }
+
+  def getEmbedLink: String = {
+    formatURL(this.getPermalinkURL + "/embed")
+  }
+
+  def getDataLink: String = {
+    formatURL(lgn.host + "/sessions/" + lgn.session + "/visualizations/" + id + "/data/")
+  }
+
+  def getHTML: String = {
+    val url = getEmbedLink
+    val request = Http(url).method("GET")
+    if (lgn.auth.nonEmpty) {
+      request.auth(lgn.auth.get._1, lgn.auth.get._2)
+    }
+    request.asString.body
+  }
+
+}
