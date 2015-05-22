bad6cb03dde999023d569a7231a5f22ec308cd40
freeman <the.freeman.lab@gmail.com>
2015-05-22 13:33:18 -0400

    Generators for random data

---
+++ src/main/scala/org/viz/lightning/types/Make.scala
@@ -0,0 +1,61 @@
+package org.viz.lightning.types
+
+import scala.util.Random
+
+object Make {
+
+  def line(t: Int): Array[Double] = {
+    Array.fill(t)(Random.nextDouble())
+  }
+
+  def lineInt(t: Int): Array[Int] = {
+    Array.fill(t)(Random.nextInt(100))
+  }
+
+  def series(n: Int, t: Int): Array[Array[Double]] = {
+    Array.fill(n)(Array.fill(t)(Random.nextDouble()))
+  }
+
+  def seriesInt(n: Int, t: Int): Array[Array[Int]] = {
+    Array.fill(n)(Array.fill(t)(Random.nextInt(100)))
+  }
+
+  def labels(n: Int): Array[Int] = {
+    Array.fill(n)(Random.nextInt(5))
+  }
+
+  def sizes(n: Int, scale: Double = 20, min: Double = 5): Array[Double] = {
+    Array.fill(n)(Random.nextFloat() * scale + min)
+  }
+
+  def alphas(n: Int, min: Double = 0.3): Array[Double] = {
+    Array.fill(n){
+      val rnd = Random.nextDouble() + 0.3
+      if (rnd > 1.0) {
+        1.0
+      } else {
+        rnd
+      }
+    }
+  }
+
+  def values(n: Int, scale: Double = 1): Array[Double] = {
+    Array.fill(n)(Random.nextDouble() * scale)
+  }
+
+  def matrix(n: Int): Array[Array[Double]] = {
+    Array.fill(n)(Array.fill(n)(Random.nextDouble()))
+  }
+
+  def sparseMatrix(n: Int, threshold: Double = 0.9): Array[Array[Double]] = {
+    Array.fill(n)(Array.fill(n){
+      val rnd = Random.nextDouble()
+      if (rnd > threshold) {
+        rnd
+      } else {
+        0.0
+      }
+    })
+  }
+
+}
