ec8c089d65745db45eaf6fec9815f41d0be285ef
freeman <the.freeman.lab@gmail.com>
2015-05-22 13:34:29 -0400

    Clean up

--- src/main/scala/org/viz/lightning/types/Three.scala
+++ src/main/scala/org/viz/lightning/types/Three.scala
@@ -4,10 +4,22 @@ import org.viz.lightning.Visualization
 
 trait Three extends Base {
 
-  def scatter3(x: Array[Double], y: Array[Double], z: Array[Double]): Visualization = {
-    val out = (x, y, z).zipped.map((x, y, z) => List(x, y, z)).toList
-    val payload = Map("points" -> out)
-    plot("scatter3", payload)
+  /**
+   * Three-dimensional data as spheres.
+   */
+  def scatter3(x: Array[Double],
+               y: Array[Double],
+               z: Array[Double],
+               label: Array[Int] = Array[Int](),
+               size: Array[Double] = Array[Double]()): Visualization = {
+
+    val points = (x, y, z).zipped.map((x, y, z) => List(x, y, z)).toList
+    val data = Map("points" -> points)
+
+    val settings = new Settings()
+      .append(List(Label(label), Size(size)))
+
+    plot("scatter3", data ++ settings.toMap)
   }
 
 }
