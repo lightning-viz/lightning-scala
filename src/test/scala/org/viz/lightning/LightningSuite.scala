05f0899c353e3b1e19dc177db1883f026292bd37
Avner Maiberg <avner.maiberg@target.com>
2015-11-22 13:37:30 -0500

    changed test assertion to test empty string instead of non-zero int

--- src/test/scala/org/viz/lightning/LightningSuite.scala
+++ src/test/scala/org/viz/lightning/LightningSuite.scala
@@ -7,7 +7,7 @@ class LightningSuite extends FunSuite {
   test("create session") {
     val lgn = Lightning("http://localhost:3000")
     lgn.createSession("test-session")
-    assert(lgn.session > 0)
+    assert(lgn.session.isEmpty)
   }
 
 }
